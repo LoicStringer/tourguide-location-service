f774174bc5fe8d13bef6c794721294dbcc873de8
Loïc Stringer <loic.stringer@gmail.com>
2020-12-04 09:04:35 +0100

    Initial commit

---
+++ src/main/java/com/tourguidelocationservice/TourguideLocationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tourguidelocationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourguideLocationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourguideLocationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tourguidelocationservice/TourguideLocationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourguidelocationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourguideLocationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
